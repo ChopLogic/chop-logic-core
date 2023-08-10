a22a4bb990f7ac9406f8d74ba49d8ecd4e86a7dc
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2023-08-10 17:32:50 +0200

    add draft tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -8,4 +8,8 @@ describe("Test", () => {
   it("testFunc 2", () => {
     expect(testFunc("test2")).toBe("test2");
   });
+
+  it("testFunc 3", () => {
+    expect(testFunc("test3")).toBe("test3");
+  });
 });
