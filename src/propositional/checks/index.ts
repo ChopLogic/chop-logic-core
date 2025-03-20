fa97b4517dc07a7506a2254d5254c3f6c1d33929
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-03-20 11:22:40 +0100

    feat: add isNegationEliminationApplicable function

--- src/propositional/checks/index.ts
+++ src/propositional/checks/index.ts
@@ -4,12 +4,15 @@ import { isImplicationEliminationApplicable } from './is-implication-elimination
 import { isDisjunctionEliminationApplicable } from './is-disjunction-elimination-applicable';
 import { isConjunctionEliminationApplicable } from './is-conjunction-elimination-applicable';
 import { isEquivalenceEliminationApplicable } from './is-equivalence-elimination-applicable';
+import { isNegationEliminationApplicable } from './is-negation-elimination-applicable';
 
 export const PropositionalChecks = {
   areStructurallyEqual: arePropFormulasStructurallyEqual,
   isWellFormed: isWellFormedFormula,
+  // Elimination rules
   isIE: isImplicationEliminationApplicable,
   isDE: isDisjunctionEliminationApplicable,
   isCE: isConjunctionEliminationApplicable,
   isEE: isEquivalenceEliminationApplicable,
+  isNE: isNegationEliminationApplicable,
 };
