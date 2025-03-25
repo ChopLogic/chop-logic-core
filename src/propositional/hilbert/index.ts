b8801fd981885b58312131ddcdf66a047be6aebf
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-03-25 20:08:04 +0100

    feat: add implicationReversal function

--- src/propositional/hilbert/index.ts
+++ src/propositional/hilbert/index.ts
@@ -1,5 +1,9 @@
 import { implicationIntroduction } from './implication-introduction';
+import { implicationDistribution } from './implication-distribution';
+import { implicationReversal } from './implication-reversal';
 
 export const HilbertAxioms = {
   II: implicationIntroduction,
+  ID: implicationDistribution,
+  IR: implicationReversal,
 };
