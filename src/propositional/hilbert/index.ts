b059aa9f2a2a2036d36a2a2f04cb6f80035261fd
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-03-26 12:10:03 +0100

    feat: add implicationElimination rule

--- src/propositional/hilbert/index.ts
+++ src/propositional/hilbert/index.ts
@@ -1,9 +1,11 @@
 import { implicationIntroduction } from './implication-introduction';
 import { implicationDistribution } from './implication-distribution';
 import { implicationReversal } from './implication-reversal';
+import { implicationElimination } from './implication-elimination';
 
-export const HilbertAxioms = {
+export const HilbertCalculus = {
   II: implicationIntroduction,
   ID: implicationDistribution,
   IR: implicationReversal,
+  IE: implicationElimination,
 };
