6fa0c58337ce1fcaee16ff08c525447ea4a27081
Dmitrii Suroviagin <Dmitrii.Suroviagin@gmail.com>
2025-03-30 20:16:40 +0200

    feat: add equivalenceIntroduction

---
+++ src/propositional/natural-calculus/__tests__/equivalence-introduction.test.ts
@@ -0,0 +1,46 @@
+import { PropFormula } from '../../../common/types';
+import { Operator } from '../../../common/enums';
+import { equivalenceIntroduction } from '../equivalence-introduction';
+
+describe('equivalenceIntroduction', () => {
+  it('should create an equivalence when given A => B and B => A', () => {
+    const formulaAtoB: PropFormula = {
+      operator: Operator.Implies,
+      values: [
+        { operator: Operator.Var, values: ['A'] },
+        { operator: Operator.Var, values: ['B'] },
+      ],
+    };
+    const formulaBtoA: PropFormula = {
+      operator: Operator.Implies,
+      values: [
+        { operator: Operator.Var, values: ['B'] },
+        { operator: Operator.Var, values: ['A'] },
+      ],
+    };
+
+    const result = equivalenceIntroduction([formulaAtoB, formulaBtoA]);
+
+    expect(result).toEqual({
+      operator: Operator.Equiv,
+      values: [
+        { operator: Operator.Var, values: ['A'] },
+        { operator: Operator.Var, values: ['B'] },
+      ],
+    });
+  });
+
+  it('should throw an error if the formulas are not valid for equivalence introduction', () => {
+    const invalidFormula: PropFormula = {
+      operator: Operator.And,
+      values: [
+        { operator: Operator.Var, values: ['A'] },
+        { operator: Operator.Var, values: ['B'] },
+      ],
+    };
+
+    expect(() => equivalenceIntroduction([invalidFormula, invalidFormula])).toThrow(
+      'Equivalence introduction is not applicable to the given formulas.',
+    );
+  });
+});
--- src/propositional/natural-calculus/index.ts
+++ src/propositional/natural-calculus/index.ts
@@ -1,5 +1,11 @@
 import { negationIntroduction } from './negation-introduction';
+import { negationElimination } from './negation-elimination';
+import { equivalenceIntroduction } from './equivalence-introduction';
 
 export const NaturalCalculus = Object.freeze({
+  // Introduction rules
   NI: negationIntroduction,
+  EI: equivalenceIntroduction,
+  // Elimination rules
+  NE: negationElimination,
 });
